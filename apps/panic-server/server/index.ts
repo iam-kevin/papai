4e84c73ab08ab0509477799ecaf3daec604e4fe2
iam-kevin <kevin.al.james@gmail.com>
2022-05-04 22:41:55 +0300

    init panic-server with svelte

---
+++ apps/panic-server/server/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import * as path from "path";
+
+const expressSvelte = require("express-svelte");
+const app = express();
+const port = 3000;
+
+// setup svelte
+app.use(
+	expressSvelte({
+		// Support legacy browsers only on production
+		legacy: process.env.NODE_ENV !== "development",
+		hydratable: true,
+		viewsDirname: __dirname + "/../views",
+		bundlesDirname: __dirname + "/../build",
+		bundlesHost: "/build",
+		bundlesPattern: "[name][extname]",
+		env: "development",
+	})
+);
+
+app.get("/", (req, res) => {
+	// @ts-ignore
+	res.svelte("index.svelte");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
