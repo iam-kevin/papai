2e3da5ff3b7301e4b9e01f6511c0e635c5dbaa0d
Kevin James <kevin.al.james@gmail.com>
2022-06-26 18:22:16 +0300

    add exports

--- papai/src/distributed/index.ts
+++ papai/src/distributed/index.ts
@@ -1,23 +1,3 @@
-import { HybridLogicalClock } from "./clock";
-import { Delta, ClockedUnit } from "./delta-based";
-import { ClockedState } from "./state-based";
-import { DistributedDataType } from "./types";
-
 export * from "./types";
-
-function delta<T extends DistributedDataType>(
-	object: T,
-	clock: HybridLogicalClock
-) {
-	const deltas = new Delta<T>(object);
-	return Array.from(deltas.units).map(
-		(d) => new ClockedUnit(d, clock.next())
-	);
-}
-
-function state<T extends DistributedDataType>(
-	object: T,
-	clock: HybridLogicalClock
-) {
-	return new ClockedState(object, clock);
-}
+export * from "./store";
+export * from "./clock";
